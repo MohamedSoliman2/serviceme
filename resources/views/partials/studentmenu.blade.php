dda33789f59489579d6a6107f8e0be15e26cd932
Zeyad-Mohamed1 <zeyadelsharawy709@gmail.com>
2025-01-23 00:42:48 +0200

    edit sidebar and web.php

--- resources/views/partials/studentmenu.blade.php
+++ resources/views/partials/studentmenu.blade.php
@@ -4,50 +4,91 @@
             <a href="{{ route('home') }}" class="i">
                 <span class="nav-icon uil uil-dashboard"></span>
                 <span class="menu-text">لوحه التحكم </span>
-               
             </a>
-
         </li>
+
         <li>
             <a href="{{ route('admin.blog.index') }}" class="i">
                 <span class="nav-icon uil uil-file"></span>
                 <span class="menu-text">المقالات</span>
-
             </a>
         </li>
+
         <li>
             <a href="{{ route('admin.testmonial.index') }}" class="i">
                 <span class="nav-icon uil uil-user"></span>
                 <span class="menu-text">اراء العملاء</span>
-
             </a>
-
         </li>
+
         <li>
             <a href="{{ route('admin.awards.index') }}" class="i">
                 <span class="nav-icon uil uil-box"></span>
                 <span class="menu-text">الجوائز</span>
-               
             </a>
-           
         </li>
+
         <li>
-            <a href="{{route('admin.about.index')}}" class="i">
+            <a href="{{ route('admin.about.index') }}" class="i">
                 <span class="nav-icon uil uil-file"></span>
                 <span class="menu-text">التحكم في صفحه عنا</span>
-               
             </a>
-           
         </li>
-        <li>
-            <a href="{{route('admin.terms.index')}}" class="i">
-                <span class="nav-icon uil uil-file"></span>
-                <span class="menu-text">الشروط والخصوصيه</span>
-               
-            </a>
-           
-        </li>
-       
 
+        <li>
+            <a href="{{ route('governorates.index') }}" class="i">
+                <span class="nav-icon uil uil-map-marker"></span>
+                <span class="menu-text">المحافظات</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('services.index') }}" class="i">
+                <span class="nav-icon uil uil-building"></span>
+                <span class="menu-text">الخدمات</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('sub-services.index') }}" class="i">
+                <span class="nav-icon uil uil-building"></span>
+                <span class="menu-text">الخدمات الفرعية</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('service-posts.index') }}" class="i">
+                <span class="nav-icon uil uil-newspaper"></span>
+                <span class="menu-text">منشورات الخدمات</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('home-posts.index') }}" class="i">
+                <span class="nav-icon uil uil-newspaper"></span>
+                <span class="menu-text">منشورات الصفحه الرئيسية</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('terms.index') }}" class="i">
+                <span class="nav-icon uil uil-file"></span>
+                <span class="menu-text">صفحة الشروط والأحكام</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('contacts.index') }}" class="i">
+                <span class="nav-icon uil uil-envelope"></span>
+                <span class="menu-text">الرسائل التي تم ارسالها</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('faqs.index') }}" class="i">
+                <span class="nav-icon uil uil-question-circle"></span>
+                <span class="menu-text">الاسئله الشائعه</span>
+            </a>
+        </li>
     </ul>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,20 @@ use App\Models\Testimonials;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\TermsAndConditionsController;
 use App\Http\Controllers\Frontend\ContactMessageController;
+use App\Http\Controllers\Frontend\FaqController;
+use App\Http\Controllers\Frontend\GovernorateController;
+use App\Http\Controllers\Frontend\HomePostsController;
+use App\Http\Controllers\Frontend\ServiceController;
+use App\Http\Controllers\Frontend\ServicePostController;
+use App\Http\Controllers\Frontend\SubServiceController;
 
 Route::get('/', function () {
-    $title="dashboard";
-    $description="dashboard";
-    $articals=Blog::count();
-    $awards=awards::count();
-    $testmonials=Testimonials::count();
-    return view('admin.dashboard',compact('title','description','articals','awards','testmonials'));
+    $title = "dashboard";
+    $description = "dashboard";
+    $articals = Blog::count();
+    $awards = awards::count();
+    $testmonials = Testimonials::count();
+    return view('admin.dashboard', compact('title', 'description', 'articals', 'awards', 'testmonials'));
 })->name('home');
 
 Route::get('/admin/blog', [BlogController::class, 'index'])->name('admin.blog.index');
@@ -35,24 +41,42 @@ Route::get('/admin/testmonial/edit/{id}', [TestmonialController::class, 'edit'])
 Route::post('/admin/testmonial/update/{id}', [TestmonialController::class, 'update'])->name('admin.testmonial.update');
 Route::post('/admin/testmonial/delete/{id}', [TestmonialController::class, 'delete'])->name('testmonial.delete');
 //awards
-Route::get('admin/awards/index',[AwardsController::class,'index'])->name('admin.awards.index');
-Route::get('admin/awards/create',[AwardsController::class,'create'])->name('admin.awards.create');
-Route::post('/admin/awards/store',[AwardsController::class,'store'])->name('admin.awards.store');
-Route::get('/admin/awards/edit/{id}',[AwardsController::class,'edit'])->name('awards.edit');
-Route::post('/admin/awards/update/{id}',[AwardsController::class,'update'])->name('admin.awards.update');
-Route::post('/admin/awards/delete/{id}',[AwardsController::class,'delete'])->name('awards.delete');
-Route::post('/upload/pui',[BlogController::class,'upload'])->name('upload');
+Route::get('admin/awards/index', [AwardsController::class, 'index'])->name('admin.awards.index');
+Route::get('admin/awards/create', [AwardsController::class, 'create'])->name('admin.awards.create');
+Route::post('/admin/awards/store', [AwardsController::class, 'store'])->name('admin.awards.store');
+Route::get('/admin/awards/edit/{id}', [AwardsController::class, 'edit'])->name('awards.edit');
+Route::post('/admin/awards/update/{id}', [AwardsController::class, 'update'])->name('admin.awards.update');
+Route::post('/admin/awards/delete/{id}', [AwardsController::class, 'delete'])->name('awards.delete');
+Route::post('/upload/pui', [BlogController::class, 'upload'])->name('upload');
 //about
-Route::get('/admin/about',[AboutController::class,'index'])->name('admin.about.index');
-Route::get('/admin/about/create',[AboutController::class,'create'])->name('about.create');
-Route::post('/admin/about/store',[AboutController::class,'store'])->name('about.store');
-Route::get('/admin/about/edit/{id}',[AboutController::class,'edit'])->name('about.edit');
-Route::post('/admin/about/update/{id}',[AboutController::class,'update'])->name('admin.about.update');
-Route::post('/admin/about/delete/{id}',[AboutController::class,'delete'])->name('about.delete');
+Route::get('/admin/about', [AboutController::class, 'index'])->name('admin.about.index');
+Route::get('/admin/about/create', [AboutController::class, 'create'])->name('about.create');
+Route::post('/admin/about/store', [AboutController::class, 'store'])->name('about.store');
+Route::get('/admin/about/edit/{id}', [AboutController::class, 'edit'])->name('about.edit');
+Route::post('/admin/about/update/{id}', [AboutController::class, 'update'])->name('admin.about.update');
+Route::post('/admin/about/delete/{id}', [AboutController::class, 'delete'])->name('about.delete');
 //terms
-Route::get('/admin/terms',[TermController::class,'index'])->name('admin.terms.index');
-Route::get('/admin/terms/create',[TermController::class,'create'])->name('terms.create');
-Route::post('/admin/terms/store',[TermController::class,'store'])->name('terms.store');
-Route::get('/admin/terms/edit/{id}',[TermController::class,'edit'])->name('terms.edit');
-Route::post('/admin/terms/update/{id}',[TermController::class,'update'])->name('admin.terms.update');
-Route::post('/admin/terms/delete/{id}',[TermController::class,'delete'])->name('terms.delete');
+Route::get('/admin/terms', [TermController::class, 'index'])->name('admin.terms.index');
+Route::get('/admin/terms/create', [TermController::class, 'create'])->name('terms.create');
+Route::post('/admin/terms/store', [TermController::class, 'store'])->name('terms.store');
+Route::get('/admin/terms/edit/{id}', [TermController::class, 'edit'])->name('terms.edit');
+Route::post('/admin/terms/update/{id}', [TermController::class, 'update'])->name('admin.terms.update');
+Route::post('/admin/terms/delete/{id}', [TermController::class, 'delete'])->name('terms.delete');
+
+Route::resource('governorates', GovernorateController::class)->except(['show']);
+
+Route::resource('services', ServiceController::class)->except(['show']);
+
+Route::resource('sub-services', SubServiceController::class)->except(['show']);
+
+Route::resource('service-posts', ServicePostController::class)->except(['show']);
+
+Route::resource('faqs', FaqController::class)->except(['show']);
+
+Route::resource('home-posts', HomePostsController::class)->except(['show']);
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::resource('terms', TermsAndConditionsController::class)->except(['create', 'edit', 'destroy', 'show']);
+});
+
+Route::resource('contacts', ContactMessageController::class)->except(['create', 'edit', 'destroy']);
