63631a7a2b2d21b79a0cf8f109623f99e34f9952
Zeyad-Mohamed1 <zeyadelsharawy709@gmail.com>
2025-01-23 00:48:39 +0200

    add some things

--- app/Http/Controllers/Admin/AwardsController.php
+++ app/Http/Controllers/Admin/AwardsController.php
@@ -10,58 +10,63 @@ use Illuminate\Http\Request;
 class AwardsController extends Controller
 {
     use image;
-    public function index(){
-       $awards=awards::paginate(15);
-       $title="all awards";
-       $description="all awards";
-       return view('admin.awards.index',compact('awards','title','description'));
+    public function index()
+    {
+        $awards = awards::paginate(15);
+        $title = "all awards";
+        $description = "all awards";
+        return view('admin.awards.index', compact('awards', 'title', 'description'));
     }
-    public function create(){
-        $title="add award";
-        $description="add award";
-        return view('admin.awards.create',compact('title','description'));
+    public function create()
+    {
+        $title = "add award";
+        $description = "add award";
+        return view('admin.awards.create', compact('title', 'description'));
     }
-    public function store(Request $request){
-    $request->validate(['title'=>['required','string','min:5','max:40'],'description'=>['required','string','min:5','max:240'],'photo'=>['required','image','file','mimes:png,jpg']]);
-    $image=$this->saveimage($request,'photo');
-    $all=array_merge($request->all(),['image'=>$image]);
-    awards::create($all);
-    return redirect()->route('admin.awards.index')->with('massage','تم اضافه الجائزه بنجاح');
+    public function store(Request $request)
+    {
+        $request->validate(['title' => ['required', 'string', 'min:5', 'max:40'], 'description' => ['required', 'string', 'min:5', 'max:240'], 'photo' => ['required', 'image', 'file', 'mimes:png,jpg']]);
+        $image = $this->saveimage($request, 'photo');
+        $all = array_merge($request->all(), ['image' => $image]);
+        awards::create($all);
+        return redirect()->route('admin.awards.index')->with('massage', 'تم اضافه الجائزه بنجاح');
     }
-    public function edit(string $id){
-        $awards=awards::where('id',$id)->first();
-        if($awards){
-            $title="edit award";
-            $description="edit  award";
-        return view('admin.awards.edit',compact('title','awards','description'));
-        }else{
-            return redirect()->route('admin.blog.index')->with('massage','هذا الرئي غير موجوده');
+    public function edit(string $id)
+    {
+        $awards = awards::where('id', $id)->first();
+        if ($awards) {
+            $title = "edit award";
+            $description = "edit  award";
+            return view('admin.awards.edit', compact('title', 'awards', 'description'));
+        } else {
+            return redirect()->route('admin.blog.index')->with('massage', 'هذا الرئي غير موجوده');
         }
+    }
+    public function update(Request $request, string $id)
+    {
+        $request->validate(['title' => ['required', 'string', 'min:5', 'max:40'], 'description' => ['required', 'string', 'min:5', 'max:240'], 'photo' => ['nullable', 'image', 'file', 'mimes:png,jpg']]);
+        $award = awards::where('id', $id)->first();
+        if ($award) {
+            $all = $request->except('photo');
+            if ($request->hasFile('photo')) {
+                $image = $this->updateimage($request, 'photo', $award->image);
+                $all = array_merge($request->all(), ['image' => $image]);
             }
-            public function update(Request $request,string $id){
-                $request->validate(['title'=>['required','string','min:5','max:40'],'description'=>['required','string','min:5','max:240'],'photo'=>['nullable','image','file','mimes:png,jpg']]);
-                $award=awards::where('id',$id)->first();
-        if($award){    
-            $all=$request->except('photo');
-    if($request->hasFile('photo')){
-        $image=$this->updateimage($request,'photo',$award->image);
-        $all=array_merge($request->all(),['image'=>$image]);  
-    }
-    $award->update($all);
-    return redirect()->route('admin.awards.index')->with('massage','تم تعديل الجائزه بنجاح');
-        }else{
-            return redirect()->route('admin.awards.index')->with('massage','هذه الجائزه غير موجوده');
+            $award->update($all);
+            return redirect()->route('admin.awards.index')->with('massage', 'تم تعديل الجائزه بنجاح');
+        } else {
+            return redirect()->route('admin.awards.index')->with('massage', 'هذه الجائزه غير موجوده');
         }
-                    }
-                    public function delete(string $id){
-                        
-                        $award=awards::where('id',$id)->first();
-                        if($award){
-                        $award->delete();
-                        return redirect()->route('admin.awards.index')->with('massage','تم حذف الجائزه بنجاح');
-                        }else{
-                            return redirect()->route('admin.awards.index')->with('massage','هذه الجائزه غير موجوده');
-                        }
-                            }
-    
+    }
+    public function delete(string $id)
+    {
+
+        $award = awards::where('id', $id)->first();
+        if ($award) {
+            $award->delete();
+            return redirect()->route('admin.awards.index')->with('massage', 'تم حذف الجائزه بنجاح');
+        } else {
+            return redirect()->route('admin.awards.index')->with('massage', 'هذه الجائزه غير موجوده');
+        }
+    }
 }
--- app/Http/Controllers/Admin/TermController.php
+++ app/Http/Controllers/Admin/TermController.php
@@ -8,50 +8,56 @@ use Illuminate\Http\Request;
 
 class TermController extends Controller
 {
-    public function index(){
-        $term=Term::get();
-        $title="terms page";
-        $description="terms page";
-        return view('admin.terms.index',compact('term','title','description'));
+    public function index()
+    {
+        $term = Term::get();
+        $title = "terms page";
+        $description = "terms page";
+        return view('admin.terms.index', compact('term', 'title', 'description'));
     }
-    public function create(){
-        $title="control About";
-        $description="control About";
-        return view('admin.terms.create',compact('title','description'));
+    public function create()
+    {
+        $title = "control About";
+        $description = "control About";
+        return view('admin.terms.create', compact('title', 'description'));
     }
-    public function store(Request $request){
-        $request->validate(['description'=>['required']]);
+    public function store(Request $request)
+    {
+        $request->validate(['description' => ['required']]);
         Term::create($request->all());
-return redirect()->route('admin.terms.index')->with('massage','تم انشاء صفحه عنا بنجاح');
+        return redirect()->route('admin.terms.index')->with('massage', 'تم انشاء صفحه عنا بنجاح');
     }
-    public function edit(string $id){
-$term=Term::first();
-if($term){
-    $title="control terms";
-    $description="control terms";
-return view('admin.terms.edit',compact('title','term','description'));
-}else{
-    return redirect()->route('admin.terms.index')->with('massage','هذه الصفحه غير موجوده');
-}
-    }
-    public function update(Request $request,string $id){
-        $request->validate(['description'=>['required']]);
-        $terms=Term::where('id',$id)->first();
-        if($terms){
-        $terms->update($request->all());
-        return redirect()->route('admin.terms.index')->with('massage','تم تعديل الصفحه بنجاح');
-        }else{
-            return redirect()->route('admin.terms.index')->with('massage','هذه الصفحه غير موجوده');
+    public function edit(string $id)
+    {
+        $term = Term::first();
+        if ($term) {
+            $title = "control terms";
+            $description = "control terms";
+            return view('admin.terms.edit', compact('title', 'term', 'description'));
+        } else {
+            return redirect()->route('admin.terms.index')->with('massage', 'هذه الصفحه غير موجوده');
         }
-            }
-            public function delete(string $id){
-                
-                $terms=Term::where('id',$id)->first();
-                if($terms){
-                $terms->delete();
-                return redirect()->route('admin.terms.index')->with('massage','تم حذف الصفحه بنجاح');
-                }else{
-                    return redirect()->route('admin.terms.index')->with('massage','هذه الصفحه غير موجوده');
-                }
-                    }
+    }
+    public function update(Request $request, string $id)
+    {
+        $request->validate(['description' => ['required']]);
+        $terms = Term::where('id', $id)->first();
+        if ($terms) {
+            $terms->update($request->all());
+            return redirect()->route('admin.terms.index')->with('massage', 'تم تعديل الصفحه بنجاح');
+        } else {
+            return redirect()->route('admin.terms.index')->with('massage', 'هذه الصفحه غير موجوده');
+        }
+    }
+    public function delete(string $id)
+    {
+
+        $terms = Term::where('id', $id)->first();
+        if ($terms) {
+            $terms->delete();
+            return redirect()->route('admin.terms.index')->with('massage', 'تم حذف الصفحه بنجاح');
+        } else {
+            return redirect()->route('admin.terms.index')->with('massage', 'هذه الصفحه غير موجوده');
+        }
+    }
 }
